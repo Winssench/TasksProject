dbb81bea73b4cd0c39dc09957ce321c16e5a3f8e
Winssench <omar.chichaoui@gmail.com>
2020-07-29 09:20:16 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , { useState } from 'react';
 import './App.css';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
+
+//React components can be either FUnctional or with class as shown below
 
 function App() {
+
+  const [courseGoals, setCourseGoals] = useState([
+    { id: 'cp1', text: 'Finish the Course'},
+    { id: 'cp2', text: 'Finish the Course d matisha'},
+    { id: 'cp3', text: 'Finish the Course dohrir'},
+  ]);
+
+
+  //push modifies the ancient array not like concat which brings new array
+  const addNewGoalHandler = (newGoal) => {
+    /** 
+    courseGoals.push(newGoal);
+    console.log(courseGoals);
+    */
+   //setCourseGoals(courseGoals.concat(newGoal)); to better schedule use this
+   setCourseGoals((precCourseGoals) => {
+     return precCourseGoals.concat(newGoal);
+   });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   //React.createElement('h1', {}, 'Hi , this is react')
+    <div className="course-goals">
+      <h2>
+        Course Goals
+      </h2>
+      <NewGoal onAddGoal={addNewGoalHandler}/>
+      <GoalList goals={courseGoals}/>
+    </div>   
   );
 }
+
+/* this is with class 
+class App extends React.Component {
+  render() {
+    return <h1>Hi, this is React !</h1>
+  }
+}
+*/
 
 export default App;
---
+++ src/components/NewGoal/NewGoal.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+
+import './NewGoal.css';
+
+
+
+const NewGoal = props => {
+
+    //let enteredText ='';
+    const [enteredText , setEnteredText] = useState('');
+    const addGoalHandler = event => {
+        event.preventDefault();
+
+        const newGoal = {
+            id: Math.random().toString(),
+            text: enteredText
+        };
+
+        //enteredText ='';
+        setEnteredText('');
+       
+       
+        props.onAddGoal(newGoal);
+    };
+
+
+    const textChangedHandler = event => {
+        setEnteredText(event.target.value);
+        //enteredText = event.target.value;
+    }
+
+    return (
+        <form className="new-goal" onSubmit={addGoalHandler}>
+            <input type="text" value={enteredText} onChange={textChangedHandler}/>
+            <button type="submit">Add Goal</button>
+        </form>
+    );
+}
+
+
+export default NewGoal;
